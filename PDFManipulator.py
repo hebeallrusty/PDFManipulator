912726ed35e6df53e17906c80907649b788366df
hebeallrusty <hebeallrusty@gmail.com>
2020-03-31 21:23:25 +0100

    add in join pdf code, attempt pdf timestamp but not quite working right

--- PDFManipulator.py
+++ PDFManipulator.py
@@ -1,9 +1,19 @@
 #import wx
 import PyPDF4 as pypdf
 import platform
+import os
+import datetime as dt
 
 pdffile = '/home/ashley/Astronomical Algorithms.pdf'
 outdir = '/home/ashley/PDFManipulator/test/'
+pdffiles = []
+for i in range (1,10):
+	pdffiles.append(f'/home/ashley/PDFManipulator/test/Astronomical Algorithms({i}).pdf')
+pdffiles.append('/home/ashley/PDFManipulator/test/OUTPUT.pdf')
+outfile = '/home/ashley/PDFManipulator/test/OUTPUT.pdf'
+
+folder = '/home/ashley/PDFManipulator/test'
+
 
 def get_pages(PDF_FILE):
 	pdf = pypdf.PdfFileReader(PDF_FILE)
@@ -38,8 +48,18 @@ def update_meta(docinfo,PDFWriterObj):
 	# TODO - make this work - or at least match the pdf version	
 	#PDFWriterObj._header.replace(b_("%PDF-1.3"),b_("%PDF-1.6"))
 	
+def pdf_timestamp():
+	# generate PDF timestamp relative to utc time
+	NOW = dt.datetime.utcnow()
+	return f'D:{NOW.year}{NOW.month}{NOW.day}{NOW.hour}{NOW.minute}{NOW.second}+01\'00\''
+
 
 def split(PDF_FILE,OUT_DIR,PageRange,dismantle = False):
+	# PDF_FILE must be a single PDF file
+	# OUT_DIR must be a folder where the files will be created
+	# PageRange must be a tuple containing start and end pages
+	# dismantle denotes whether single files are required (True) or one file containing the range of pages (False)
+
 	print(f'Opening {PDF_FILE}')
 	pdf = pypdf.PdfFileReader(PDF_FILE)
 	print(f'Getting Document Properties')
@@ -77,8 +97,51 @@ def split(PDF_FILE,OUT_DIR,PageRange,dismantle = False):
 				with open(output,'wb') as out:
 					pdf_writer.write(out)
 					out.close
-		
+def files_in_folder(FOLDER):
+	# case sensitive so lowercase all ext and then test
+	return [os.path.join(FOLDER,FILE) for FILE in os.listdir(FOLDER) if FILE.lower().endswith(".pdf")]
 	
-#print(get_docinfo(pdffile))
-split(pdffile,outdir,(1,2),True)
+
+def join(PDF_FILES,OUT_FILENAME,folder=False):
+	# PDF_FILES must be a list of files or a folder of pdf files (must use folder=True to denote Folder)
+	# OUT_FILENAME is the file that will be created
+	# folder is the switch to denote tha PDF_FILES is actually a folder
+	
+	# initiate the writer object
+	pdf_writer = pypdf.PdfFileWriter()
+	# if we are dealing with a folder - get all the pdf's in that folder and convert that into the PDF_FILES
+	if folder == True:
+		print(f'getting all PDF files in {PDF_FILES}')
+		PDF_FILES = files_in_folder(PDF_FILES)
+
+	for PDF in PDF_FILES:
+		print(f'Opening {PDF}')
+		# get a writer object
+		pdf = pypdf.PdfFileReader(PDF)
+		# we need all the pages to add to the writer object in the page writer loop
+		pages = get_pages(PDF)
+		print(f'{PDF} has {pages} pages')
+		
+		for PAGE in range(pages):
+			print(f'Dealing with page {PAGE+1} in {PDF}')
+			# add each page to the writer
+			pdf_writer.addPage(pdf.getPage(PAGE))
+
+	# TODO make this work properly - put in the creation date and modified date as today
+	print(f'{pdf_timestamp()}')
+	meta = pdf_writer._info.getObject()
+	meta.update({pypdf.generic.NameObject('/CreationDate'): pypdf.generic.createStringObject(pdf_timestamp())})
+	meta.update({pypdf.generic.NameObject('/ModDate'): pypdf.generic.createStringObject(pdf_timestamp())})
+	
+	print(f'Writing {OUT_FILENAME}')
+	with open(OUT_FILENAME,'wb') as out:
+		pdf_writer.write(out)
+
+
+print(get_docinfo(pdffile))
+#split(pdffile,outdir,(1,get_pages(pdffile)),True)
 #get_filename(pdffile)
+join(folder,outfile,True)
+#files_in_folder(folder)
+print(get_docinfo(outfile))
+#print(pdf_datetime())
